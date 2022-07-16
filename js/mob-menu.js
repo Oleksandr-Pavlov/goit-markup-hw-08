165b58728e9f1d4dc3988aca94e8b23a45006a66
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-07-16 13:18:02 +0300

    upd

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -4,10 +4,12 @@
     closeMenuBtn: document.querySelector('.menu-btn--close'),
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
+    contact: document.querySelector('[data-contacts]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.contact.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
